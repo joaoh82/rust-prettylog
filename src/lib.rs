939f3dd9ab1b5e3bdc93f6ba2c74dc8041393b3d
Joao Henrique Machado Silva <jmachadosilva@oneconcern.com>
2021-01-03 16:30:04 -0300

    testing first crate publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+/// Takes a message `&str` as input and
+/// Prints <p style='color:red;'>🤦 ERROR : message</p> 
+///
+/// # Examples
+///
+/// ```
+/// prettylog::error("This is an error message!");
+/// ``` 
+pub fn error(message: &str) {
+    println!("🤦  \x1B[1;31mERROR :\x1B[0m \x1B[31m{}\x1B[0m", message);
 }
